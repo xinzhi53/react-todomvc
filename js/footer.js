b3b2392d8df2f6bdbb142817f6c58b1bd37c6ac9
lisi <2909628008@qq.com>
2016-11-20 16:44:13 +0800

    <react>

---
+++ js/footer.js
@@ -0,0 +1,23 @@
+(function(){
+		var Footer = React.createClass({
+		render: function() {
+			return <footer className="footer">
+				<span className="todo-count"><strong>{this.props.leftTodos}</strong> item left</span>
+				<ul className="filters">
+					<li>
+						<a className="selected" href="#/">All</a>
+					</li>
+					<li>
+						<a href="#/active">Active</a>
+					</li>
+					<li>
+						<a href="#/completed">Completed</a>
+					</li>
+				</ul>
+				<button className="clear-completed">Clear completed</button>
+			</footer>
+		}
+	});
+		window.Footer = Footer;
+		// return Footer;
+})()
